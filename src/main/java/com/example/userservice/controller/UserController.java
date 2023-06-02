db9e873b057ad82f6099719f04cc3b402cb582e3
Bactv <bactv@rabiloo.com>
2023-06-02 16:40:53 +0700

    update db

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("user")
 public class UserController {
     private final UserService userService;
 
